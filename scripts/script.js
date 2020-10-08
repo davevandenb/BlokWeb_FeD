12c33f7161bf561895d5dce9ad4c9c86d0f71f5f
Dave van den Broek <71263972+davevandenb@users.noreply.github.com>
2020-10-08 15:53:11 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,55 @@
 // JavaScript Document
 
 var hamburger = document.querySelector("nav button:last-of-type");
-var navigation = document.querySelector("nav ul")
-var hamburgerImg = document.querySelector("nav button:last-of-type img")
+var navigation = document.querySelector("nav ul");
+var shippingInfo = document.querySelector("header section:first-of-type button img")
 
 
 hamburger.addEventListener("click", toonNav);
 
 function toonNav() {
+    var hamburgerImg = document.querySelector("nav button:last-of-type img");
+    console.log(navigation)
+    if (navigation.classList.contains('zichtbaar')) {
+        hamburgerImg.src = "images/contents/icon_menu.png";
+    }
+    else {
+        hamburgerImg.src = "images/contents/icon_exit.png";
+    } 
     navigation.classList.toggle('zichtbaar');
+    
+    
+ // if element    
+   
+  // MAAK 2 FUNCTIONS
 }
+
+
+
+
+var shippingSection = document.querySelector(".test");
+
+function hideShipping() {
+    shippingSection.classList.add('.ontzichtbaar');
+    
+}
+shippingInfo.addEventListener("click", hideShipping);
+
+
+
+// werkt 1 kant op: alleen van hamburg naar kruis:
+///////////////////
+// hamburger.addEventListener("click", toonNav);
+//
+// function toonNav() {
+//     navigation.classList.toggle('zichtbaar');
+//     var hamburgerImg = document.querySelector("nav button:last-of-type img");
+//     hamburgerImg.src = "images/contents/icon_exit.png"
+// }
+
+
+
+
 
 
 // Hamburger icoon toggle
